fd7cb3af3812809dfa0676e4e2ba44167d372021
Melo <jmmelo.edu@gmail.com>
2026-04-01 07:43:25 -0400

    Add files via upload

---
+++ test_lab_game.py
@@ -0,0 +1,13 @@
+from unittest.mock import Mock
+from lab_game import get_random_item, award_badge
+
+def test_get_random_item():
+    fake_rng = Mock()
+    fake_rng.choice.return_value = "sword"
+    result = get_random_item("Aria", rng = fake_rng)
+    assert result == "Aria found a sword"
+
+def test_award_badge():
+    mock_logger = Mock ()
+    award_badge("Aria", "Hero", mock_logger)
+    mock_logger.log.assert_called_once_with("Aria earned Hero")
